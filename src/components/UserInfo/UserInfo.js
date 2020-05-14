fd8ab67a8a7435d482b19b4b3dda49e636246454
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-14 17:02:26 -0600

    :tada: Initial commit

---
+++ src/components/UserInfo/UserInfo.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./UserInfo.css";
+
+const UserInfo = (props) => {
+   
+    const { username, usage, favoriteListings, logout } = props
+    return (
+      <div>
+        <p className="user-info-text">
+          Welcome {username}, we hope you find something for your {usage} needs. You have {favoriteListings.length} favorite listings.
+        </p>
+        
+            <button onClick={logout}>Logout</button>
+      </div>
+    )
+}
+
+export default UserInfo;
+
+UserInfo.propTypes = {};
