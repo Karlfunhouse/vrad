09433a845d7f88c0394f3dac8da8a2cfac52ad3e
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-05-14 16:26:25 -0600

    :crystal_ball: Adding file paths in ListingContainer component to display images for each listing; currently link is broken but we are close

--- src/components/ListingInfo/ListingInfo.js
+++ src/components/ListingInfo/ListingInfo.js
@@ -3,14 +3,21 @@ import PropTypes from 'prop-types'
 import './ListingInfo.css'
 
 const ListingInfo = (props) => {
+    console.log(props.listing)
+    const images = props.listing.img.map(img => {
+        console.log(img)
+        return <img src={`../../../../public/images/${img}`} />
+    })
     const { area, area_id } = props.listing 
     const { baths, beds, cost_per_night, superhost} = props.listing.details
     return (
         <div>ListingInfo
+            <div>
+                { images }
+            </div>
             <p>Area: {area}</p>
             <p>Area Id: {area_id}</p>
-            <p>Bath: {baths
-            }</p>
+            <p>Bath: {baths}</p>
             <p>Beds: {beds}</p>
             <p>$ {cost_per_night}</p>
             <p>Superhost: {superhost}</p>
