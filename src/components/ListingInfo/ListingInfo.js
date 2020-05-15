4f0f0a74a37372b9e7a64a658337b0ec871e46d3
Ed Ignot <edignot@gmail.com>
2020-05-14 21:55:22 -0600

    :art Basic slider gallery

--- src/components/ListingInfo/ListingInfo.js
+++ src/components/ListingInfo/ListingInfo.js
@@ -5,21 +5,26 @@ import './ListingInfo.css'
 const ListingInfo = (props) => {
     console.log(props.listing)
     const images = props.listing.img.map(img => {
-        return <img alt='' src={`/images/${img}`} />
+        return <img 
+        alt='' 
+        className='listing-info-img'
+        src={`/images/${img}`} />
     })
     const { area, area_id } = props.listing 
     const { baths, beds, cost_per_night, superhost} = props.listing.details
     return (
         <div className='listing-info-container'>
-            <div>
-                { images }
-            </div>
             <p>Area: {area}</p>
             <p>Area Id: {area_id}</p>
             <p>Bath: {baths}</p>
             <p>Beds: {beds}</p>
             <p>$ {cost_per_night}</p>
             <p>Superhost: {superhost}</p>
+            <div className='slide-gallery'>
+                <figure className='slider'>
+                    { images }
+                </figure>
+            </div>
         </div>
     )
 }
