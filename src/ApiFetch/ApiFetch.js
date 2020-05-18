6678b808e4b56a391029e28c49881ca0d500fb2e
Ed Ignot <edignot@gmail.com>
2020-05-18 17:31:15 -0600

    :crystal_ball: Created separate file for FetchApi and running fetch through cleaning function inside App component

---
+++ src/ApiFetch/ApiFetch.js
@@ -0,0 +1,67 @@
+import imageData from '../imageData'
+
+export const fetchAreas = async () => {
+    try {
+        const url = "https://vrad-api.herokuapp.com";
+        let areasData = [];
+        const response = await fetch(url + "/api/v1/areas");
+        const areas = await response.json();
+        areas.areas.forEach(async (area) => {
+          const response2 = await fetch(url + area.details);
+          const areaDetails = await response2.json();
+          const areaObject = {
+            ...areaDetails,
+            area: area.area,
+          };
+          areasData.push(areaObject);
+        });
+        return areasData
+      } catch (e) {
+        console.log(e);
+      }
+}
+
+export const fetchListings = async (listings) => {
+    const url = "https://vrad-api.herokuapp.com";
+                    const listingsPromises = listings.map((listing) => {
+                      const images = Object.entries(imageData).find((item) => {
+                        return (item[0] === listing.split("").splice(17).join("")) && item[1]
+                      });
+                      return fetch(url + listing)
+                        .then((response) => response.json())
+                        .then((listing) => {
+                          return {
+                            favorite: false,
+                            ...listing,
+                            img: images[1],
+                          };
+                        });
+                    });
+                    return Promise.all(listingsPromises)
+                      .catch((err) => console.error(err));
+}
+
+
+
+ //  displayListings = async (listings) => {
+                //   try {
+                //     const url = "https://vrad-api.herokuapp.com";
+                //     let listingsArray = [];
+                //     await listings.forEach(async (listing) => {
+                //       const images = Object.entries(imageData).find((item) => {
+                //         return (item[0] === listing.split("").splice(17).join("")) && item[1]
+                //       });
+                //       const response = await fetch(url + listing)
+                //       const listingDetails = await response.json();
+                //       const listingObject = {
+                //             favorite: false,
+                //             ...listingDetails,
+                //             img: images[1],
+                //           }
+                //       listingsArray.push(listingObject)
+                //     })
+                //     this.setState({ listings: listingsArray })
+                //   } catch (e) {
+                //     console.log(e)
+                //   } 
+                // };
