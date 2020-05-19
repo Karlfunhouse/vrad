7472672695f8f0b3b120d802665c52b411c68651
Ed Ignot <edignot@gmail.com>
2020-05-19 09:33:11 -0600

    :crystal_ball: Header test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Header from './Header';
+import React from 'react'
+import { render } from '@testing-library/react'
+import Header from './Header'
 
-test('<Header/>', () => {
-  expect(true).toBeTruthy()
-});
+test('<Header/> component successfully renders', () => {
+  const { getByText } = render(<Header />)
+  expect(getByText('Vacation Rentals Around Denver')).toBeInTheDocument()
+})
--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -4,48 +4,44 @@ import Login from './Login'
 
 const handleSubmit = jest.fn()
 
-let login
+let loginComponent
 beforeEach(() => {
-  login = render(<Login/>)
+  loginComponent = render(<Login/>)
 })
 
 afterEach(cleanup)
 
 test('that LOGIN button is disabled if any input is empty', () => {
-  const { getByLabelText, getByText } = login
+  const { getByLabelText, getByText } = loginComponent
   getByLabelText('Username:').value = ''
   expect(getByText('LOGIN')).toHaveAttribute('disabled');
 })
 
 test('that LOGIN button is enabled if all inputs', () => {
-  const { getByLabelText, getByText, debug } = login
-  getByLabelText('Username:').value = 'user'
-  fireEvent.change(getByLabelText('Username:'))
-  getByLabelText('Email:').value = 'email'
-  fireEvent.change(getByLabelText('Email:'))
-  getByLabelText('Business:').value = 'business'
-  fireEvent.click(getByLabelText('Business:'))
+  const { getByLabelText, getByText, debug } = loginComponent
+  fireEvent.change(getByLabelText('Username:'), {target: {value: 'user'}})
+  fireEvent.change(getByLabelText('Email:'), {target: {value: 'email'}})
   handleSubmit()
   expect(getByText('LOGIN')).toHaveAttribute('disabled');
   debug()
 })
 
 test('that username value is changed', () => {
-  const { getByLabelText } = login
+  const { getByLabelText } = loginComponent
   getByLabelText('Username:').value = 'user'
   fireEvent.change(getByLabelText('Username:'))
   expect(getByLabelText('Username:').value).toBe('user')
 })
 
 test('that email value is changed', () => {
-  const { getByLabelText } = login
+  const { getByLabelText } = loginComponent
   getByLabelText('Email:').value = 'email'
   fireEvent.change(getByLabelText('Email:'))
   expect(getByLabelText('Email:').value).toBe('email')
 })
 
 test('that handle submit is called', () => {
-  const { getByText } = login
+  const { getByText } = loginComponent
   fireEvent.click(getByText('LOGIN'))
   handleSubmit({
     email: 'email',
@@ -58,3 +54,4 @@ test('that handle submit is called', () => {
     usage: 'business'
   })
 })
+
