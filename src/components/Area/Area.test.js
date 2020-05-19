77916c175f3d651429ed5d1f15ba9fb1ce7a0d23
Ed Ignot <edignot@gmail.com>
2020-05-18 19:16:46 -0600

    :crystal_ball: Refactored Area test

--- src/components/Area/Area.test.js
+++ src/components/Area/Area.test.js
@@ -1,52 +1,50 @@
-import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
-import Area from "./Area";
-import { BrowserRouter } from "react-router-dom";
+import React from "react"
+import { render, cleanup, fireEvent } from "@testing-library/react"
+import Area from "./Area"
+import { BrowserRouter } from "react-router-dom"
 
-const area = {
-  about: "RiNo is a burgeoning area with new bars",
-  area: "RiNo",
-  id: 590,
-  listings: ["/api/v1/listings/3", "/api/v1/listings/44"],
-  location: "North of Downtown Denver",
-  name: "River North",
-  quick_search: "o5kod9f5cqo0",
-  region_code: 6356834,
-};
-const displayListings = jest.fn();
+const displayListings = jest.fn()
 
-afterEach(cleanup);
+let areaComponent
+let area
+beforeEach(() => {
+  area = {
+    about: "about",
+    area: "area",
+    id: 1,
+    listings: ["/1", "/2"],
+    location: "location",
+    name: "name",
+    quick_search: "1",
+    region_code: 1,
+  }
+  areaComponent = render(
+    <BrowserRouter>
+      <Area area={area} key={area.id} displayListings={displayListings} />
+    </BrowserRouter>
+  )
+})
+
+afterEach(cleanup)
 
 test("component successfully renders", () => {
-  const { getByText } = render(
-    <BrowserRouter>
-      <Area area={area} key={area.id} displayListings={displayListings} />
-    </BrowserRouter>
-  );
-  expect(getByText("RiNo")).toBeInTheDocument();
-});
+  const { getByText } = areaComponent
+  expect(getByText("about")).toBeInTheDocument()
+})
 
 test("that displayListings has been clicked", () => {
-  const { getByText, debug } = render(
-    <BrowserRouter>
-      <Area area={area} key={area.id} displayListings={displayListings} />
-    </BrowserRouter>
-  );
-  fireEvent.click(getByText("See Listings"));
-  fireEvent.click(getByText("See Listings"));
-  expect(displayListings).toHaveBeenCalledTimes(2);
-});
+  const { getByText } = areaComponent
+  fireEvent.click(getByText("See Listings"))
+  fireEvent.click(getByText("See Listings"))
+  expect(displayListings).toHaveBeenCalledTimes(2)
+})
 
 test("that displayListings has been called with listings", () => {
-  const { getByText, debug } = render(
-    <BrowserRouter>
-      <Area area={area} key={area.id} displayListings={displayListings} />
-    </BrowserRouter>
-  );
-  fireEvent.click(getByText("See Listings"));
-  expect(displayListings).toHaveBeenCalledWith(area.listings);
-});
+  const { getByText } = areaComponent
+  fireEvent.click(getByText("See Listings"))
+  expect(displayListings).toHaveBeenCalledWith(area.listings)
+})
 
 test("that the url pathway changes when user clicks button", () => {
-  expect(location.pathname).toBe("/areas/RiNo/listings");
-});
+  expect(location.pathname).toBe(`/areas/${area.area}/listings`)
+})
